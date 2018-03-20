4126c2b59d481a656397d5e8113196a9b3061709
star <15970795078@163.com>
2018-03-20 14:45:13 +0800

    现在初步的对数据清理这个文件进行了功能模块划分，接下来，尝试着把他们写到一个类里

--- dataoptions.py
+++ dataoptions.py
@@ -1,24 +1,27 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
 import cx_Oracle
 import pandas as pd
 import re
-from oracleoptions import save_to_oracle
+# from oracleoptions import save_to_oracle
 
-conn = cx_Oracle.connect("star/star@127.0.0.1/orcl")
+def getData():
 
-sql = 'select * from zhaopin'
-sql = 'select * from zhaopin where rownum<=100'
-sql = 'select * from zhaopin_city'
+    conn = cx_Oracle.connect("star/star@127.0.0.1/orcl")
 
-# Index(['网址', '工作名称', '公司名称', '公司网址', '福利', '月工资', '发布日期', '经验', '人数', '工作地点',
-#        '工作性质', '最低学历', '职位类别', '公司规模', '公司性质', '公司行业', '职位描述', '是否失效'])
-df = pd.read_sql(sql=sql, con=conn)
-conn.close()
+    sql = 'select * from zhaopin_city'
+
+    # Index(['网址', '工作名称', '公司名称', '公司网址', '福利', '月工资', '发布日期', '经验', '人数', '工作地点',
+    #        '工作性质', '最低学历', '职位类别', '公司规模', '公司性质', '公司行业', '职位描述', '是否失效'])
+    df = pd.read_sql(sql=sql, con=conn)
+    conn.close()
+    return df
 
 # 筛选java从业职位
-java = [d for d in df.工作名称 if 'JAVA' in d or 'Java' in d or 'java' in d]
+# java = [d for d in df.工作名称 if 'JAVA' in d or 'Java' in d or 'java' in d]
 
 
-def getCity(df):
+def clearCity(df):
     # 提取城市
     t = '北京-朝阳区'
     re.sub(r'-\S*$', '', t)
@@ -32,17 +35,19 @@ def getCity(df):
 # print(df)
 
 
-# df = getCity(df)
+# df = clearCity(df)
 # # 那下一次就直接从数据库里取zhaopin_city了
 # save_to_oracle(df, "zhaopin_city")
 
 
-# 统计工作地点信息
-print(dict(df.工作地点.value_counts()))
+# 统计职位数分布情况
+# 返回字典
+def getJobsNum(df):
+    return dict(df.工作地点.value_counts())
 
 
-# 找出各职位的招聘信息条数
-def getJobNum(df):
+# 找出各职位的招聘信息
+def getXXJobNum(df):
     def jobNum(job):
         # 找出java职位数
         # len(list(filter(lambda s: 'JAVA' in s or 'Java' in s or 'java' in s, df.工作名称)))
@@ -70,10 +75,10 @@ def getJobNum(df):
 # 这是一张三维图
 
 # 找出各职位的具体招聘信息
-def jobinfo(job):
+def jobinfo(df, job):
     # re.I: 正则忽略大小写
     if 'c++' == job or 'C++' == job:
-        job = 'c\+\+'+
+        job = 'c\+\+'
     elif 'c' == job or 'C' == job:
         job += '[^+#]'
     p = re.compile(job, re.I)
@@ -85,8 +90,8 @@ def jobinfo(job):
     tmp = df.loc[filter(lambda s: re.search(p, s), df.工作名称)]
     return tmp
 
-# 找出各职位的具体招聘信息
-def getJobInfo(job):
+# 找出某职位的具体招聘信息
+def getJobInfo(df, job):
     def process(x,job):
         # "java"/"JAVA"/"Java"
         if job in x.工作名称 or job.upper() in x.工作名称 or job.capitalize() in x.工作名称:
@@ -95,4 +100,7 @@ def getJobInfo(job):
             return False
     # 利用apply方法，已经可以提取出特定招聘信息的条目了
     return df[df.apply(process, axis=1,args=([job]))]
-# print(getJobInfo('c#'))
+
+
+# df=getData()
+# print(getJobInfo(df,'c#'))
