8b825c6afd612e78c1fd15a0e0fdb5222cb72bdf
star <15970795078@163.com>
2018-05-22 17:06:33 +0800

    把统计所有城市、职位信息的统计都放到数据库模块 统计完毕后存入Oracle，以备后续使用

--- oracleoptions.py
+++ oracleoptions.py
@@ -57,14 +57,22 @@ def save_to_oracle_v0(df, database=''):
                  '{} VARCHAR(256), {} VARCHAR(256), {} VARCHAR(256), {} VARCHAR(256), ' \
                  '{} VARCHAR(256), {} VARCHAR(256), {} VARCHAR(256), {} VARCHAR(256),' \
                  '{} VARCHAR(256), {} VARCHAR(3889), {} VARCHAR(8))'.format(database, str(df.columns[0]),
-                                                                            str(df.columns[1]),
-                                                                            str(df.columns[2]), str(df.columns[3]),
-                                                                            str(df.columns[4]), str(df.columns[5]),
-                                                                            str(df.columns[6]), str(df.columns[7]),
-                                                                            str(df.columns[8]), str(df.columns[9]),
-                                                                            str(df.columns[10]), str(df.columns[11]),
-                                                                            str(df.columns[12]), str(df.columns[13]),
-                                                                            str(df.columns[14]), str(df.columns[15]),
+                                                                            str(df.columns[
+                                                                                1]),
+                                                                            str(df.columns[2]), str(
+                                                                                df.columns[3]),
+                                                                            str(df.columns[4]), str(
+                                                                                df.columns[5]),
+                                                                            str(df.columns[6]), str(
+                                                                                df.columns[7]),
+                                                                            str(df.columns[8]), str(
+                                                                                df.columns[9]),
+                                                                            str(df.columns[10]), str(
+                                                                                df.columns[11]),
+                                                                            str(df.columns[12]), str(
+                                                                                df.columns[13]),
+                                                                            str(df.columns[14]), str(
+                                                                                df.columns[15]),
                                                                             str(df.columns[16]), str(df.columns[17]))
 
         cursor.execute(create)  # 如果表不存在,创建呗
@@ -85,9 +93,12 @@ def save_to_oracle_v0(df, database=''):
     for i in range(len(df)):
         try:
             cursor.execute(insert % (df.iloc[i, 0], df.iloc[i, 1], df.iloc[i, 2], df.iloc[i, 3],
-                                     df.iloc[i, 4], df.iloc[i, 5], df.iloc[i, 6], df.iloc[i, 7],
-                                     df.iloc[i, 8], df.iloc[i, 9], df.iloc[i, 10], df.iloc[i, 11],
-                                     df.iloc[i, 12], df.iloc[i, 13], df.iloc[i, 14], df.iloc[i, 15],
+                                     df.iloc[i, 4], df.iloc[i, 5], df.iloc[
+                                         i, 6], df.iloc[i, 7],
+                                     df.iloc[i, 8], df.iloc[i, 9], df.iloc[
+                                         i, 10], df.iloc[i, 11],
+                                     df.iloc[i, 12], df.iloc[i, 13], df.iloc[
+                                         i, 14], df.iloc[i, 15],
                                      df.iloc[i, 16], df.iloc[i, 17]))
         except [cx_Oracle.DatabaseError, UnicodeEncodeError] as e:
             print('写入失败')
@@ -95,7 +106,8 @@ def save_to_oracle_v0(df, database=''):
             for j in range(len(df)):
                 print(df.iloc[i, j])
         except UnicodeEncodeError:
-            print("'gbk' codec can't encode character xxx in position 173: illegal multibyte sequence")
+            print(
+                "'gbk' codec can't encode character xxx in position 173: illegal multibyte sequence")
             print('i=' + str(i))
             for j in range(len(df)):
                 print(df.iloc[i, j])
@@ -106,7 +118,7 @@ def save_to_oracle_v0(df, database=''):
 
 
 # 这个测试添加日志
-def save_to_oracle(df, database=''):
+def save_to_oracle_var(df, database=''):
     # 创建日志文件
     logger = log.log(file=database)
     conn = cx_Oracle.connect("star/star@127.0.0.1/orcl")
@@ -124,14 +136,22 @@ def save_to_oracle(df, database=''):
                  '{} VARCHAR(256), {} VARCHAR(256), {} VARCHAR(256), {} VARCHAR(256), ' \
                  '{} VARCHAR(256), {} VARCHAR(256), {} VARCHAR(256), {} VARCHAR(256),' \
                  '{} VARCHAR(256), {} VARCHAR(3889), {} VARCHAR(8))'.format(database,
-                                                                            str(df.columns[0]), str(df.columns[1]),
-                                                                            str(df.columns[2]), str(df.columns[3]),
-                                                                            str(df.columns[4]), str(df.columns[5]),
-                                                                            str(df.columns[6]), str(df.columns[7]),
-                                                                            str(df.columns[8]), str(df.columns[9]),
-                                                                            str(df.columns[10]), str(df.columns[11]),
-                                                                            str(df.columns[12]), str(df.columns[13]),
-                                                                            str(df.columns[14]), str(df.columns[15]),
+                                                                            str(df.columns[0]), str(
+                                                                                df.columns[1]),
+                                                                            str(df.columns[2]), str(
+                                                                                df.columns[3]),
+                                                                            str(df.columns[4]), str(
+                                                                                df.columns[5]),
+                                                                            str(df.columns[6]), str(
+                                                                                df.columns[7]),
+                                                                            str(df.columns[8]), str(
+                                                                                df.columns[9]),
+                                                                            str(df.columns[10]), str(
+                                                                                df.columns[11]),
+                                                                            str(df.columns[12]), str(
+                                                                                df.columns[13]),
+                                                                            str(df.columns[14]), str(
+                                                                                df.columns[15]),
                                                                             str(df.columns[16]), str(df.columns[17]))
 
         cursor.execute(create)  # 如果表不存在,创建呗
@@ -152,15 +172,58 @@ def save_to_oracle(df, database=''):
     for i in range(len(df)):
         try:
             cursor.execute(insert % (df.iloc[i, 0], df.iloc[i, 1], df.iloc[i, 2], df.iloc[i, 3],
-                                     df.iloc[i, 4], df.iloc[i, 5], df.iloc[i, 6], df.iloc[i, 7],
-                                     df.iloc[i, 8], df.iloc[i, 9], df.iloc[i, 10], df.iloc[i, 11],
-                                     df.iloc[i, 12], df.iloc[i, 13], df.iloc[i, 14], df.iloc[i, 15],
+                                     df.iloc[i, 4], df.iloc[i, 5], df.iloc[
+                                         i, 6], df.iloc[i, 7],
+                                     df.iloc[i, 8], df.iloc[i, 9], df.iloc[
+                                         i, 10], df.iloc[i, 11],
+                                     df.iloc[i, 12], df.iloc[i, 13], df.iloc[
+                                         i, 14], df.iloc[i, 15],
                                      df.iloc[i, 16], df.iloc[i, 17]))
         except (cx_Oracle.DatabaseError, UnicodeEncodeError) as e:
             # 如果写数据库失败,这里会保存系统抛出的报错
             # 同时,也会保存导致出错的信息
             logger.saveInfo(e, df.iloc[i])
     conn.commit()
+
+    cursor.close()
+    conn.close()
+
+
+def save_to_oracle_int(df, database=""):
+    conn = cx_Oracle.connect("star/star@127.0.0.1/orcl")
+    cursor = conn.cursor()  # 创建游标对象
+
+    # database = 'city_job'
+
+    # obj = dp.DataOptions()
+    # df = obj.statisticalAlltoOracle()
+    columns = list(df.columns)  # ['北京', '深圳', '广州', '上海']
+    index = list(df.index)  # ['Linux', 'IOS', 'PHP']
+
+    try:  # 表是否存在
+        cursor.execute('select count(*) from {}'.format(database))
+    except cx_Oracle.DatabaseError:
+        print(database + '不存在')
+        createSQL = 'create table ' + database + ' ( '
+        for city in columns:
+            createSQL = createSQL + city + '    int, '
+        createSQL = createSQL[:-2] + ')'  # 去掉最后的', '，加上')'
+        print(createSQL)
+        cursor.execute(createSQL)  # 如果表不存在,创建呗
+    print(database + '存在')
+
+    insert = "insert into " + database + " ("
+    for city in columns:
+        insert = insert + city + ", "  # 在这里面换行时会被添加进空格，导致`ORA-00917: 缺失逗号 Oracle`
+    insert = insert[:-2] + ") values ("
+    for job in index:
+        insertSQL = insert
+        for city in columns:
+            insertSQL = insertSQL + str(df[city][job]) + ', '
+        insertSQL = insertSQL[:-2] + ")"  # 把最后的', '两个字符去掉
+        print(insertSQL)
+        cursor.execute(insertSQL)
+        conn.commit()
 
     cursor.close()
     conn.close()
